3b591a25ebbf309efcb05d7767aed57b8080ce78
SMushy14 <s.mushy1@alustudent.com>
2025-02-21 10:48:56 +0000

    deleted files

--- sms_analysis/db/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS transactions (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date TEXT,
-    transaction_type TEXT,
-    amount INTEGER,
-    sender_receiver TEXT,
-    new_balance INTEGER,
-    transaction_id TEXT
-);
--- sms_analysis/scripts/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Transaction(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    transaction_type = db.Column(db.String(100))
-    amount = db.Column(db.Float)
-    timestamp = db.Column(db.DateTime)
